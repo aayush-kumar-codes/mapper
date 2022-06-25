d85b4978af5b05c1f5ed599699b1af197b3f8307
ashutosh <ashutoh.excel2011@gmail.com>
2022-06-25 18:14:20 +0530

    fixed

--- funding/serializers.py
+++ funding/serializers.py
@@ -30,7 +30,14 @@ class DataPointsSerializer(serializers.ModelSerializer):
         for day in days:
             # Get all the fundings of day passed in array
             calculated_time = datetime.now() - timedelta(days=day)
-            total_rate = FundingBase.objects.filter(time__gte=calculated_time, future=obj.future).aggregate(Sum('rate'))
+            # total_rate = FundingBase.objects.filter(time__gte=calculated_time, future=obj.future).aggregate(Sum('rate'))
+            try:
+                fundings = FundingBase.objects.filter(future=obj.future).order_by('-time')
+                fundings = fundings[0: 24*day]
+            except IndexError:
+                pass
+            total_rate = fundings.aggregate(Sum('rate', distinct=True))
+            
             try:
                 total_rate_list.append(((total_rate['rate__sum'] / 24) * 365 * 24 * 100))
             except TypeError:
--- settings/views.py
+++ settings/views.py
@@ -1,5 +1,3 @@
 from django.shortcuts import render
-from rest_framework.response import Response
-from settings.models import Settings
 
 # Create your views here.
