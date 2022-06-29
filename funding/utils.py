1f0605fc694bf7c19fadc1ba340433792257cb2b
ashutosh <ashutoh.excel2011@gmail.com>
2022-06-29 13:01:36 +0800

    changes pushed

--- funding/utils.py
+++ funding/utils.py
@@ -57,6 +57,6 @@ def remove_funding_before_60_days():
 
 def Cronjob():
     scheduler = BackgroundScheduler(timezone=str(get_current_timezone()))
-    scheduler.add_job(get_funding, trigger='interval', minutes=1)
+    scheduler.add_job(get_funding, trigger='interval', minutes=30)
     scheduler.add_job(remove_funding_before_60_days, trigger='interval', hours=24)
     scheduler.start()
